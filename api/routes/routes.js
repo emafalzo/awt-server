b49bcf83a5165f4e7a9b3367b7419cf22efa8a56
emanuele <emanuele.falzone@yahoo.it>
2017-07-12 15:16:57 +0200

    Fixed security warning

--- api/routes/routes.js
+++ api/routes/routes.js
@@ -14,10 +14,10 @@ module.exports = function(app) {
         res.setHeader('Access-Control-Allow-Origin', '*');
 
         // Request methods you wish to allow
-        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
 
         // Request headers you wish to allow
-        res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Access-Control-Allow-Methods ,Access-Control-Allow-Origin, Access-Control-Allow-Headers, Authorization, X-Requested-With');
+        res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Access-Control-Allow-Methods ,Access-Control-Allow-Origin, Access-Control-Allow-Headers, authorization, X-Requested-With');
 
         // Pass to next layer of middleware
         next();
